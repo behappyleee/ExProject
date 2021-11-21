f9f232ff07cebc1cabbecbdc9af1a8e03e82505c
lee <dhfhfkwhdk@naver.com>
2021-11-21 23:08:26 +0900

    Project Commit

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -33,14 +33,14 @@ function LandingPage() {
 
     return (
         <Col lg={6} md={8} xs={24}>
-            <a href={`/video/pose/${video._id}`}>
                 <div style={{ position: 'relative' }}>
+                <a href={`/video/${video._id}`}>
                     <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt />
                     <div className="duration">
                         <span> { minutes } : { seconds } </span>
                     </div>
+                    </a>
                 </div>
-            </a>
         <br/>
         <Meta
             avatar= {
---
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import {Row, Col, List, Avatar} from 'antd';
+import Axios from 'axios';
+import { response } from 'express';
+import { post } from '../../../../../server/routes/video';
+
+function VideioDetailPage(props) {
+    
+    useEffect(() => {
+
+        const videoId = props.match.params.videoId
+        const variable = { videoId: videoId }
+
+        const [VideoDetail, setVideoDetail ] = useState([]);
+
+       Axios.post('/api/video/getVideoDetail', variable)
+            .then(reponse => {
+                if(response.data.success) {
+                    setVideoDetail( response.data.VideoDetail )
+                } else {
+                    alert('비디오 정보를 가져오기 실패하였습니다.');
+                }
+            })
+    }, [])
+    
+    if(VideoDetail.writer) {
+        return (
+            <Row gutter={[16, 16]}>
+                <Col lg ={18} xs={25}>
+                     <div style={{ width: '100%', padding:'3rem 4rem' }}>
+                         <video style={{ width: '100%'  }}  src={ `http://localhost:5000/${VideoDetail.filePath} `} />
+                     <List.Item
+                         actions
+                     >
+                         <List.Item.Meta
+                             avatar={<Avatar src={ VideoDetail.writer.image } />}
+                             title={ post.writer.name }
+                             description={ VideoDetail.description }
+                         />
+     
+                        
+                     </List.Item>
+                     {/* Comments */}
+                     
+                     
+                     
+                     </div>              
+     
+                </Col>
+                <Col lg={6} xs={24}>
+                     Side Videos 
+     
+                </Col>
+            </Row>
+         )
+    } else {
+        return (
+            <div> .... Loading </div>
+        )
+    }
+    
+    // return (
+    //    <Row gutter={[16, 16]}>
+    //        <Col lg ={18} xs={25}>
+    //             <div style={{ width: '100%', padding:'3rem 4rem' }}>
+    //                 <video style={{ width: '100%'  }}  src={ `http://localhost:5000/${VideoDetail.filePath} `} />
+    //             <List.Item
+    //                 actions
+    //             >
+    //                 <List.Item.Meta
+    //                     avatar={<Avatar src={ VideoDetail.writer.image } />}
+    //                     title={ post.writer.name }
+    //                     description={ VideoDetail.description }
+    //                 />
+
+                   
+    //             </List.Item>
+    //             {/* Comments */}
+                
+                
+                
+    //             </div>              
+
+    //        </Col>
+    //        <Col lg={6} xs={24}>
+    //             Side Videos 
+
+    //        </Col>
+    //    </Row>
+    // )
+}
+
+export default VideioDetailPage
